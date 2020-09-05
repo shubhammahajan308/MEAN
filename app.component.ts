34ef409aec168d5a7bdee17e2f60bf1bb7383ca3
shubham Mahavir Mahajan <65168569+shubhammahajan308@users.noreply.github.com>
2020-09-05 22:29:47 +0530

    my-app files.

---
+++ app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-app3';
+  inputTask = new FormControl();
+  taskList: any = [];
+
+  addTask() {
+    const newtask = this.inputTask.value;
+    this.taskList.push(newtask);
+
+    this.inputTask.setValue("");
+  }
+}
